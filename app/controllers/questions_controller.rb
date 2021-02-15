208b2f50a4c466b0b33b432b4c64422e9c7d6994
Fernando Gomes <gomesrodriguesfernando@gmail.com>
2021-02-15 16:07:56 -0300

    done

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,17 @@
+class QuestionsController < ApplicationController
+  def ask
+    
+  end
+
+  def answer
+    @answer = ''
+    @user_input = params[:question]
+    if @user_input == 'I am going to work'
+      @answer = 'Great!'
+    elsif @user_input.include? '?'
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
